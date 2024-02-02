291a66651dfd2c424c850712cf25e5b1958fb1ba
IannisL <Iannislopez720@gmail.com>
2024-02-02 11:48:36 -0500

    second commit

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,27 @@
-import { Router } from "express";
+import {Router} from 'express';
 
 const router = new Router();
 
-// POST SIGN IN
-router.post('/signin', async(req, res) => {
-// Check if user exist
-// check if password is a match
-// send thr db user
-const user = {_id: '1', email: 'alex@gmail.com', username: 'alex123'};
-res.json(user);
+/**
+ * POST /signin
+ */
+router.post('/signin', async (req, res) => {
+    // check if user exist
+    // check if password is a match
+    // send the db user
+    const user = {_id: '1', email: 'alex@gmail.com', userName: 'alex123'};
+    res.json(user);
 });
-// POST SIGN UP
-router.post('/signup', async(req, res) => {
+
+/**
+ * POST /signup
+*/
+router.post('/signup', async (req, res) => {
     // check email is not in db
     // create a new user in db
     // send the new user
-    const user = {_id: '1', email: 'alex@gmail.com', username: 'alex123'};
-res.json(user);
+    const user = {_id: '1', email: 'alex@gmail.com', userName: 'alex123'};
+    res.json(user);
 });
-
 
 export default router;
